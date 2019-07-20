9caf447df73edf7d68ba649cf32e3b5c54d1c86f
racheldmiller <rachelmiller415@gmail.com>
2019-07-19 17:25:11 -0700

    had to pass in option to get MongoClient.connect to work

--- server.js
+++ server.js
@@ -1,18 +1,42 @@
-// Dependencies
-var express = require("express");
+// Other dependencies I have to do more research about
+var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var logger = require("morgan");
 
-// See if I need morgan or not?
+// initialize express app
+var express = require("express");
+var app = express();
 
 // Scraping tools
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
-var db = require("./models");
+// create local port
+var PORT = process.env.PORT || 8080;
 
-// Initialize Express
-var app = express();
+// set up logger
+app.use(logger("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+// handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+// create mongoose database connection
+mongoose.connect("mongodb://localhost:27017/scrape-news", {
+  useNewUrlParser: true
+});
+var db = mongoose.connection;
+
+// log any errors that happens during connction
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to Mongoose!");
+});
 
 // Start the server
 app.listen(PORT, function() {
